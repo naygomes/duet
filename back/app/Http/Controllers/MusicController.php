b98749d84387f150d39a6e7c44966831c38552da
Nayara Gomes <nayara.gomes13@poli.ufrj.br>
2021-02-14 00:51:52 -0300

    + criando controller e rotas para primeiro teste

--- back/app/Http/Controllers/MusicController.php
+++ back/app/Http/Controllers/MusicController.php
@@ -4,7 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Music;
+
 class MusicController extends Controller
 {
-    //
+    public function saveMusic(Request $request) {
+        $music = new Music();
+        $music->saveMusic($request);
+        return response()->json($music);
+    }
 }
